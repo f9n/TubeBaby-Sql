b05f4fe960001ebaa04de5f78883959e27627c82
pleycpl <h4rvey@riseup.net>
2016-11-10 10:56:19 +0300

    Adding project's schema

---
+++ tupBebek-schema.sql
@@ -0,0 +1,101 @@
+
+DROP TABLE Hastane;
+CREATE TABLE Hastane (
+	hastane_no			varchar(12) not null,
+	hastane_ismi		varchar(12) not null,
+	hastane_adresi	varchar(20) not null,
+	hastane_tel			varchar(12) not null,
+	primary key (hastane_no)
+);
+
+DROP TABLE Doktor;
+CREATE TABLE Doktor (
+	dr_ad		 		varchar(15) not null,
+	dr_soyad 		varchar(15) not null,
+	dr_no				varchar(12) not null,
+	hastane_no	varchar(12) not null,
+	dr_cep_tel	varchar(12),
+	primary key	(dr_no),
+	foreign key (hastane_no) references Hastane(hastane_no)
+);
+
+DROP TABLE Hasta;
+CREATE TABLE Hasta (
+	hasta_ad		 	varchar(15) not null,
+	hasta_soyad 	varchar(15) not null,
+	hasta_tc_no		varchar(12) not null,
+	hasta_bdate		date,
+	hasta_cep_tel	varchar(12),
+	hasta_sex			varchar,
+	dr_no					varchar(12) not null,
+	primary key (hasta_tc_no),
+	foreign key (dr_no) references Doktor(dr_no)
+);
+
+DROP TABLE HastaEsi;
+CREATE TABLE HastaEsi (
+	es_ad		 		varchar(15) not null,
+	es_soyad 		varchar(15) not null,
+	es_tc_no		varchar(12) not null,
+	es_bdate		date,
+	es_cep_tel	varchar(12),
+	es_sex			char,
+	hasta_tc_no	varchar(15) not null,
+	primary key (es_tc_no),
+	foreign key (hasta_tc_no) references Hasta(hasta_tc_no)
+);
+
+DROP TABLE Aile;
+CREATE TABLE Aile (
+	--aile_no				    smallint,	-- aile no kavramina gerek yok?	
+																-- istersek foreign keyleri birlestirebiliriz.					
+	a_evlilik_tarihi	date,
+	a_hasta_tc				varchar(12) not null,
+	a_es_tc						varchar(12) not null, 		
+	aile_adress				varchar(30),
+	a_haveAchild			boolean default false,
+	primary key (a_hasta_tc, a_es_tc),
+	foreign key (a_hasta_tc) references Hasta(hasta_tc_no),
+	foreign key (a_es_tc) references HastaEsi(es_tc_no)
+);
+
+DROP TABLE Odalar;
+CREATE TABLE Odalar (
+	hastane_no				varchar(12) not null,
+	dr_no			        varchar(12), -- illa odanin dolu olmasi gerekmiyor.
+	oda_no				    varchar(12) not null,
+	kat								char not null,
+	primary key (oda_no),
+	foreign key (hastane_no) references Hastane(hastane_no),
+	foreign key (dr_no)  references Doktor(dr_no)
+);
+
+DROP TABLE Telefon_info;
+CREATE TABLE Telefon_info (
+	hastane_no		varchar(12) not null,
+	dr_no					varchar(12), -- illa odanin dolu olmasi gerekmiyor.
+	oda_no				varchar(12) not null,
+	tel_no				varchar(12) not null,
+	primary key (tel_no),
+	foreign key (hastane_no) references Hastane(hastane_no),
+	foreign key (dr_no) 	 references Doktor(dr_no),
+	foreign key (oda_no) 	 references Odalar(oda_no)
+);
+
+DROP TABLE Ilac;
+CREATE TABLE Ilac (
+	ilac_no					varchar(12) not null,
+	ilac_ismi				varchar(12) not null,
+	ilac_turu				varchar(30),
+	primary key (ilac_no)
+);
+
+DROP TABLE Ilac_Hasta;
+CREATE TABLE Ilac_Hasta (
+	hasta_tc_no			varchar(12),
+	ilac_no					varchar(12),
+	primary key (hasta_tc_no, ilac_no),
+	foreign key (hasta_tc_no) references Hasta(hasta_tc_no),
+	foreign key (ilac_no) references Ilac(ilac_no)
+);
+
