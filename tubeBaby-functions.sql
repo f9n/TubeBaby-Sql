6e1e2a64eedae43a0c5383bb0ca6ed43d521f21b
pleycpl <h4rvey@riseup.net>
2016-12-28 18:57:26 +0300

    Changed old table names

--- tubeBaby-functions.sql
+++ tubeBaby-functions.sql
@@ -1,43 +1,43 @@
 -- 1-) Doktor tablosunda en yüksek ve en düşük maaş arasındaki farkı bulan fankşin  -> Buton ile calıstıracagiz
-CREATE OR REPLACE FUNCTION odev4fonk1 () returns real AS '
+CREATE OR REPLACE FUNCTION odev4fonk1 () returns real AS $$
 DECLARE
 	fark_maas real;
-	max_maas Doktor.dr_salary%TYPE;
-	min_maas Doktor.dr_salary%TYPE;
+	max_maas Doctor.dr_salary%TYPE;
+	min_maas Doctor.dr_salary%TYPE;
 BEGIN
 					SELECT MIN(dr_salary) INTO min_maas FROM Doktor;
 					SELECT MAX(dr_salary) INTO max_maas FROM Doktor;
 					fark_maas := max_maas - min_maas;
 					RETURN fark_maas;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-SELECT odev4fonk1();
+-- SELECT odev4fonk1();
 
 -- 2-)Hangi doktorun hic hastasi yoktur. -> Buton ile calistiracagiz.
 
 
-CREATE OR REPLACE FUNCTION odev4fonk3 () returns Doktor.dr_ad%TYPE AS '
+CREATE OR REPLACE FUNCTION odev4fonk3 () returns Doctor.dr_ad%TYPE AS $$
 DECLARE
-	isim Doktor.dr_ad%TYPE;
+	isim Doctor.dr_ad%TYPE;
 BEGIN
-					SELECT d.dr_ad INTO isim FROM Doktor AS d WHERE NOT EXISTS (SELECT * FROM Hasta AS h WHERE h.dr_no=d.dr_no);
+					SELECT d.dr_name INTO isim FROM Doctor AS d WHERE NOT EXISTS (SELECT * FROM Patinent AS p WHERE p.dr_no = d.dr_no);
 					RETURN isim;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-SELECT odev4fonk3();
+-- SELECT odev4fonk3();
 
 -- 3-)adı girilen doktorun hasta sayısını bulan fonsiyon -> Guı den parametre alıcaz.
 
-CREATE OR REPLACE FUNCTION odev4fonk2 (drad Doktor.dr_ad%TYPE) returns int AS '
+CREATE OR REPLACE FUNCTION odev4fonk2 (drad Doctor.dr_name%TYPE) returns int AS $$
 DECLARE
 	adet int;
 BEGIN
-					SELECT COUNT(*) INTO adet FROM Doktor d, Hasta h WHERE d.dr_no=h.dr_no AND d.dr_ad=drad;
+					SELECT COUNT(*) INTO adet FROM Doctor d, Patient p WHERE d.dr_no = p.dr_no AND d.dr_ad = drad;
 					RETURN adet;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-SELECT odev4fonk2('Yasin');
+-- SELECT odev4fonk2('Yasin');
 
