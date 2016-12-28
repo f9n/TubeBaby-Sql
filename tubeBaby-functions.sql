be8224df8b1a43d762ecf426cbde971ebe681e99
Sarah Walker <h4rvey@protonmail.com>
2016-12-28 18:14:15 +0200

    Update tubeBaby-functions.sql

--- tubeBaby-functions.sql
+++ tubeBaby-functions.sql
@@ -43,5 +43,29 @@ $$ LANGUAGE 'plpgsql';
 -- SELECT odev4fonk2('Yasin');
 
 -------------------------------------------------Triggers
+--1-) bi doktorun maaşına değişiklik yapılınca 10000 altında bütün maaşlar 2 katına çıkıyo
+---fonksiyonu
 
+CREATE OR REPLACE FUNCTION trig_fonk_ornek1() RETURNS TRIGGER AS '
+BEGIN
+update Doctor set dr_salary = new.dr_salary where dr_salary<10000;
+
+RETURN NEW;
+END;
+' LANGUAGE 'plpgsql';
+
+-------trigeri
+
+CREATE TRIGGER ornek1 AFTER update ON Doctor
+FOR EACH ROW EXECUTE PROCEDURE trig_fonk_ornek1();
+
+----------tetikleyici herhangi komut
+
+UPDATE Doctor SET dr_salary = 3000 WHERE Doctor.dr_name='Yılmaz';
+
+-----------fonk ve trigeri düşürme komutları
+
+--DROP TRIGGER ornek1 on Doktor;
+
+--DROP FUNCTION trig_fonk_ornek1();
 
